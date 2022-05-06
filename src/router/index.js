62a74f32d772ed44b20e2e87b425f4a0f913d91e
ponochovny <ponochovny@gmail.com>
2022-05-07 02:59:46 +0300

    prettier, updated firebase post product, changed basic chars array

--- src/router/index.js
+++ src/router/index.js
@@ -7,33 +7,33 @@ import AddProduct from '../Pages/AddProduct.vue'
 import EditProduct from '../Pages/EditProduct.vue'
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        {
-            path: '',
-            component: Main
-        },
-        {
-            path: '/auth',
-            component: Auth
-        },
-        {
-            path: '/new-product',
-            component: AddProduct,
-            beforeEnter: AuthGuard
-        },
-        {
-            path: '/edit-product/:id',
-            component: EditProduct,
-            beforeEnter: AuthGuard
-        }
-    ],
-    scrollBehavior (to, from, savedPosition) {
-        if (savedPosition) {
-            return savedPosition
-        }
-        return { left: 0, top: 0 }
-    }
+	history: createWebHistory(),
+	routes: [
+		{
+			path: '',
+			component: Main,
+		},
+		{
+			path: '/auth',
+			component: Auth,
+		},
+		{
+			path: '/new-product',
+			component: AddProduct,
+			beforeEnter: AuthGuard,
+		},
+		{
+			path: '/edit-product/:id',
+			component: EditProduct,
+			beforeEnter: AuthGuard,
+		},
+	],
+	scrollBehavior(to, from, savedPosition) {
+		if (savedPosition) {
+			return savedPosition
+		}
+		return { left: 0, top: 0 }
+	},
 })
 
 export default router
