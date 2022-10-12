c3d3255aab38ad9a1b3e76a53604f4a4cd64fb73
ponochovny <ponochovny@gmail.com>
2022-10-12 03:58:00 +0300

    check route change for login error message

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,22 @@
 <script lang="ts">
 import Header from './components/Header/Header.vue'
 import Preloader from './components/Preloader/Preloader.vue'
+import { useRouter } from 'vue-router'
+import { useToast } from 'vue-toastification'
 
 export default {
   name: 'app',
   components: {
     Header,
     Preloader,
+  },
+  setup() {
+    const router = useRouter()
+    const toast = useToast()
+
+    router.afterEach((to, _) => {
+      if (to.query.loginError) toast.error('Please log in to access this page')
+    })
   },
 }
 </script>
