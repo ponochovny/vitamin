cf38a5fdf3cb0b37c77e46fbb89efcd0a9740ba0
ponochovny <ponochovny@gmail.com>
2022-10-29 00:55:47 +0300

    profile page started

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -1,143 +1,87 @@
 <template>
   <div class="Profile">
-    <div class="Profile__title">Profile</div>
-    <Spinner v-if="isLoading" />
-    <div class="Profile__form" v-if="filledChars">
-      <button
-        type="button"
-        class="btn"
-        @click="addColumn"
-        :style="{ marginBottom: '20px' }"
-      >
-        Add column
-      </button>
-      <div
-        class="fieldsetGroup wl-15 wi-10"
-        v-for="(value, char) of charsValues"
-        :key="char"
-      >
-        <div class="fieldset__title">{{ value }}</div>
-        <div
-          class="fieldset"
-          v-for="(item, i) of filledChars[char]"
-          :key="item"
-        >
-          <div class="fieldset__label">{{ item.title }}</div>
-          <input
-            type="number"
-            min="0"
-            class="fieldset__input"
-            v-for="(item, j) of versionsCounter"
-            :key="item"
-            v-model.number.lazy="filledChars[char][i].values[j].value"
-          />
+    <div class="Profile__wrapper">
+      <div class="Profile__setChars">
+        <div class="Profile__subtitle">
+          <span>Set characteristics</span>
+          <span
+            @click="autoCalculation = !autoCalculation"
+            :class="autoCalculation ? 'active' : ''"
+          >
+            <svg
+              width="19"
+              height="6"
+              viewBox="0 0 19 6"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M13.3333 3C13.3333 4.47276 14.5272 5.66667 16 5.66667C17.4728 5.66667 18.6667 4.47276 18.6667 3C18.6667 1.52724 17.4728 0.333333 16 0.333333C14.5272 0.333333 13.3333 1.52724 13.3333 3ZM0 3.5L16 3.5V2.5L0 2.5L0 3.5Z"
+                :fill="autoCalculation ? 'var(--black)' : '#D0D0D0'"
+              />
+            </svg>
+            Auto-calculation</span
+          >
+        </div>
+        <div class="Profile__groups">
+          <div class="Profile__group">
+            <div class="Profile__group-title">Energy</div>
+            <div class="Profile__group-row">
+              <span>Energy, kkal</span>
+              <input type="text" value="10" />
+            </div>
+            <div class="Profile__group-row">
+              <span>Energy, kkal</span>
+              <input type="text" value="10" />
+            </div>
+            <div class="Profile__group-row">
+              <span>Energy, kkal</span>
+              <input type="text" value="10" />
+            </div>
+            <div class="Profile__group-row">
+              <span>Energy, kkal</span>
+              <input type="text" value="10" />
+            </div>
+          </div>
+          <div class="Profile__group">
+            <div class="Profile__group-title">Vitamins</div>
+            <div class="Profile__group-row">
+              <span>Energy, kkal</span>
+              <input type="text" value="10" />
+            </div>
+            <div class="Profile__group-row">
+              <span>Energy, kkal</span>
+              <input type="text" value="10" />
+            </div>
+            <div class="Profile__group-row">
+              <span>Energy, kkal</span>
+              <input type="text" value="10" />
+            </div>
+            <div class="Profile__group-row">
+              <span>Energy, kkal</span>
+              <input type="text" value="10" />
+            </div>
+          </div>
+          <button class="btn btn-accent btn-p2">Save</button>
         </div>
       </div>
-      <!-- Source start -->
-
-      <div class="fieldsetGroup wl-15 wi-10">
-        <div class="fieldset__title">Sources:</div>
-        <div class="fieldset">
-          <div class="fieldset__label"></div>
-          <input
-            type="text"
-            class="fieldset__input"
-            v-for="(item, j) of versionsCounter"
-            :key="item"
-            v-model.lazy="
-              filledChars[Object.keys(filledChars)[0]][0].values[j].origin
-            "
-          />
-        </div>
-      </div>
-      <!-- SOURCE end -->
-      <button class="btn" type="button" @click="dispatchUpdateData">
-        Update
-      </button>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { computed, ref, watch, onMounted } from 'vue'
-import { values } from '../constants/chars'
-import { maxAmountVersions, fullFilledChars } from '../helper'
 import Spinner from '../components/Spinner/Spinner.vue'
-import { useMainStore } from '../stores'
+import { ref } from 'vue'
 
 export default {
   components: {
     Spinner,
   },
   setup() {
-    const wait = ref(false)
-    const isLoading = ref(false)
-
-    const filledChars = ref(null)
-
-    const versionsCounter = ref(1)
-    const charsValues = ref(values)
-
-    const dispatchUpdateData = () => {
-      if (filledChars.value === null) return alert('nonono')
-
-      useMainStore().updateUserChars(filledChars.value)
-    }
-
-    const fill = () => {
-      filledChars.value = fullFilledChars(useMainStore().userChars)
-
-      if (useMainStore().userChars) {
-        // @ts-ignore
-        versionsCounter.value = maxAmountVersions(useMainStore().userChars)
-      }
-    }
-
-    const addColumn = () => {
-      for (const [key, _] of Object.entries(charsValues.value)) {
-        // @ts-ignore
-        filledChars.value[key] = filledChars.value[key].map((item) => {
-          const newValues = [...item.values]
-          newValues.push({ origin: '', value: null })
-          return {
-            ...item,
-            values: [...newValues],
-          }
-        })
-      }
-
-      versionsCounter.value = versionsCounter.value + 1
-    }
-
-    const userCharsVar = computed(() => useMainStore().userChars)
-
-    onMounted(() => {
-      if (useMainStore().user === null) {
-        wait.value = true // set flag for refill
-        isLoading.value = true
-      } else {
-        fill()
-      }
-    })
-
-    watch(userCharsVar, (newValue, oldValue) => {
-      console.log(`Updating from ${oldValue} to ${newValue}`)
-      if (oldValue === null && wait.value) {
-        fill()
-        isLoading.value = false
-      }
-    })
+    const autoCalculation = ref(false)
 
     return {
-      wait,
-      isLoading,
-      filledChars,
-      versionsCounter,
-      charsValues,
-      userCharsVar,
-
-      dispatchUpdateData,
-      addColumn,
+      autoCalculation,
     }
   },
 }
@@ -145,14 +89,52 @@ export default {
 
 <style lang="scss">
 .Profile {
-  width: 100%;
-  max-width: 1024px;
-  margin: 0 auto;
-  margin-top: 20px;
-  margin-bottom: 20px;
-  &__title {
-    font-size: 36px;
-    margin-bottom: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  padding-top: 34px;
+  &__wrapper {
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    justify-content: space-between;
+
+    max-width: $wrapper_maxWidth;
+    width: $wrapper_width;
+  }
+  &__subtitle {
+    font-weight: 400;
+    font-size: 24px;
+    line-height: 28px;
+
+    display: flex;
+    align-items: center;
+    gap: 12px;
+
+    span {
+      display: flex;
+      align-items: center;
+      &:nth-child(2) {
+        gap: 12px;
+        color: #d0d0d0;
+
+        transition: color 0.35s ease;
+        svg {
+          path {
+            transition: fill 0.35s ease;
+          }
+        }
+
+        &.active {
+          color: var(--black);
+        }
+
+        &:hover {
+          cursor: pointer;
+        }
+      }
+    }
   }
 }
 </style>
