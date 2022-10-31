014f57189df9cb9a736a0837bf4ad5b44071aea6
ponochovny <ponochovny@gmail.com>
2022-10-31 23:28:22 +0200

    small refactor of main file

--- src/main.ts
+++ src/main.ts
@@ -18,31 +18,29 @@ import { useUserStore } from './stores/modules/user'
 
 createApp({
   created() {
-    console.log('...[created]')
-    const firebaseConfig = {
-      apiKey: import.meta.env.VITE_API_KEY,
-      authDomain: `${import.meta.env.VITE_PROJECT_ID}.firebaseapp.com`,
-      databaseURL: `https://${import.meta.env.VITE_PROJECT_ID}.firebaseio.com`,
-      projectId: import.meta.env.VITE_PROJECT_ID,
-      storageBucket: `${import.meta.env.VITE_PROJECT_ID}.appspot.com`,
-      messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-      appId: import.meta.env.VITE_APP_ID,
-    }
-    // Initialize Firebase
-    const firebaseApp = initializeApp(firebaseConfig)
-    const firebaseAuth = getAuth(firebaseApp)
-
-    onAuthStateChanged(firebaseAuth, (user) => {
-      if (user) {
-        useUserStore().autoLoginUser(user)
-        useUserStore().setUserChecked()
-        console.log('...[created] Loged In')
-      } else {
-        useUserStore().setUserChecked()
-        console.log('...[created] No User Logged In')
+    if (!import.meta.env.VITE_USE_MOCK_DATA) {
+      console.log('...[created]')
+      const firebaseConfig = {
+        apiKey: import.meta.env.VITE_API_KEY,
+        authDomain: `${import.meta.env.VITE_PROJECT_ID}.firebaseapp.com`,
+        databaseURL: `https://${
+          import.meta.env.VITE_PROJECT_ID
+        }.firebaseio.com`,
+        projectId: import.meta.env.VITE_PROJECT_ID,
+        storageBucket: `${import.meta.env.VITE_PROJECT_ID}.appspot.com`,
+        messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+        appId: import.meta.env.VITE_APP_ID,
       }
-    })
+      // Initialize Firebase
+      const firebaseApp = initializeApp(firebaseConfig)
+      const firebaseAuth = getAuth(firebaseApp)
 
+      onAuthStateChanged(firebaseAuth, (user) => {
+        if (user) useUserStore().autoLoginUser(user)
+      })
+    }
+
+    useUserStore().setUserChecked()
     useMainStore().fetchProducts()
   },
   render: () => h(App),
